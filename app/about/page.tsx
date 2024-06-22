b7668063850426ed1889311de0fb26cf85f11edc
Jake Ryu <jake.ryu@outlook.com>
2024-06-22 18:12:33 +0100

    Update avatar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -32,7 +32,7 @@ export default async function AboutPage() {
       <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
         <div className="min-w-48 max-w-48 flex flex-col gap-2">
           <Avatar className="h-48 w-48">
-            <AvatarImage src="/avatar.png" alt={siteConfig.author} />
+            <AvatarImage src="/astronaut.jpeg" alt={siteConfig.author} />
             <AvatarFallback>JC</AvatarFallback>
           </Avatar>
           <h2 className="text-2xl font-bold text-center break-words">
